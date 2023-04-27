b35a2af5ec4e5e5751306eea73aa4489d11ee3d4
Sasta Dev <89509397+SastaDev@users.noreply.github.com>
2023-04-27 21:26:32 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bencode',
+    version='0.0.1',
+    author='Sasta Dev',
+    description='A python package to encode and decode bencode.',
+    license='MIT',
+    packages=find_packages()
+)
